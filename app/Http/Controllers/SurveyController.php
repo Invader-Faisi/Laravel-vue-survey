8eaafd621e2b82f0a9f7e6e53b8a466f09fc782e
Invader-Faisi <code.pedia.tube@gmail.com>
2024-01-07 10:26:05 +0500

    save image to database

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -6,7 +6,12 @@ use App\Models\Survey;
 use App\Http\Requests\StoreSurveyRequest;
 use App\Http\Requests\UpdateSurveyRequest;
 use App\Http\Resources\SurveyResource;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use function PHPSTORM_META\type;
+use function PHPUnit\Framework\matches;
 
 class SurveyController extends Controller
 {
@@ -25,9 +30,17 @@ class SurveyController extends Controller
      */
     public function store(StoreSurveyRequest $request)
     {
-        $result = Survey::create($request->validated());
+        $data = $request->validated();
 
-        return new SurveyResource($result);
+        // check if data contain data
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        return new SurveyResource($survey);
     }
 
     /**
@@ -47,7 +60,22 @@ class SurveyController extends Controller
      */
     public function update(UpdateSurveyRequest $request, Survey $survey)
     {
-        $survey->update($request->validated());
+        $data = $request->validated();
+
+        // check if data contain data
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+
+            // If there is an old image, delete it
+            if ($survey->image) {
+                $absolutePath = public_path($survey->image);
+                File::delete($absolutePath);
+            }
+        }
+
+        $survey->update($data);
+        return new SurveyResource($survey);
     }
 
     /**
@@ -61,5 +89,37 @@ class SurveyController extends Controller
         }
         $survey->delete();
         return response(content: '', status: 204);
+    }
+
+    private function saveImage($image)
+    {
+        // check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // getting the base64 encoded text without mine type
+            $image = substr($image, offset: strpos($image, ',') + 1);
+            $type = strtolower($type[1]);
+
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception(message: 'Invalid Image Type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image == false) {
+                throw new \Exception(message: 'base64_decode failed');
+            }
+        } else {
+            throw new \Exception(message: 'did not match data URI with image data');
+        }
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+        return $relativePath;
     }
 }
